15cf63b7afcc0ab6289e29f5c57455c19c16ce1f
Zacharia Brian Rangkuti <zakariabrian86@gmail.com>
2026-04-24 11:09:22 +0700

    Fix typos in databaseProyek description

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ const databaseProyek = {
         gambar: "https://i.pinimg.com/1200x/ff/bd/dd/ffbddd37b2ceffe750fc534bfa11429d.jpg",
         latar: "Ini adalah proyek magang saya dimana saya mempelajari & membuat dasar sebuah website menggunakan HTML beserta CSS.",
         tantangan: "Ketikdatahuan saya akan coding dan dasar website yang mengharuskan saya agar belajar lebih rajin.",
-        pelajaran: "Bahwa coding itu adalah hal yang menyenangkan dan mudah untuk diingat asalkan kita mau mengulang nya.",
+        pelajaran: "Bahwa coding itu adalah hal yang menyenangkan dan mudah untuk diingat asalkan kita mau mempelajari nya.",
         link: ""
     },
 };
