65b60fb3ed5f7b23fe654f60a426b7b1a1730c98
Zacharia Brian Rangkuti <zakariabrian86@gmail.com>
2026-04-24 10:51:08 +0700

    Update databaseProyek structure and content

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 const databaseProyek = {
-    "sistem_post": {
-        judul: "sistem post untuk PT AC MILAN",
+    "dasar pemrograman": {
+        judul: "mempelajari dasar pemrograman",
         keahlian: ["HTML"],
-        gambar: "https://upload.wikimedia.org/wikipedia/commons/d/d0/Logo_of_AC_Milan.svg",
-        latar: "ini adalah proyek magang saya dimana saya membuat sistem post untuk PT AC MILAN",
-        tantangan: "ajfkajfgn",
-        pelajaran: "afaggggzhasdf",
+        gambar: "https://i.pinimg.com/1200x/ff/bd/dd/ffbddd37b2ceffe750fc534bfa11429d.jpg",
+        latar: "ini adalah proyek magang saya dimana saya mempelajari & membuat dasar sebuah website menggunakan HTML beserta CSS.",
+        tantangan: "ketikdatahuan saya akan coding dan dasar website yang mengharuskan saya agar belajar lebih rajin.",
+        pelajaran: "bahwa coding itu adalah hal yang menyenangkan dan mudah untuk diingat asalkan kita mau mengulang nya.",
         link: ""
     },
 };
