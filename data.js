15cf7fc99843750c2e080591065eceb62a82e427
Zacharia Brian Rangkuti <zakariabrian86@gmail.com>
2026-04-24 10:56:30 +0700

    Update databaseProyek with new skills and capitalization

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 const databaseProyek = {
     "dasar pemrograman": {
-        judul: "mempelajari dasar pemrograman",
-        keahlian: ["HTML"],
+        judul: "Mempelajari Dasar Pemrograman",
+        keahlian: ["HTML", "CSS"],
         gambar: "https://i.pinimg.com/1200x/ff/bd/dd/ffbddd37b2ceffe750fc534bfa11429d.jpg",
-        latar: "ini adalah proyek magang saya dimana saya mempelajari & membuat dasar sebuah website menggunakan HTML beserta CSS.",
-        tantangan: "ketikdatahuan saya akan coding dan dasar website yang mengharuskan saya agar belajar lebih rajin.",
-        pelajaran: "bahwa coding itu adalah hal yang menyenangkan dan mudah untuk diingat asalkan kita mau mengulang nya.",
+        latar: "Ini adalah proyek magang saya dimana saya mempelajari & membuat dasar sebuah website menggunakan HTML beserta CSS.",
+        tantangan: "Ketikdatahuan saya akan coding dan dasar website yang mengharuskan saya agar belajar lebih rajin.",
+        pelajaran: "Bahwa coding itu adalah hal yang menyenangkan dan mudah untuk diingat asalkan kita mau mengulang nya.",
         link: ""
     },
 };
