84a5f5bbc19055b7f6844b6e261205f6ca3c0bfe
Zacharia Brian Rangkuti <zakariabrian86@gmail.com>
2026-04-24 09:23:01 +0700

    upload github pertamaku

---
+++ data.js
@@ -0,0 +1,11 @@
+const databaseProyek = {
+    "sistem_post": {
+        judul: "sistem post untuk PT AC MILAN",
+        keahlian: ["HTML"],
+        gambar: "https://upload.wikimedia.org/wikipedia/commons/d/d0/Logo_of_AC_Milan.svg",
+        latar: "ini adalah proyek magang saya dimana saya membuat sistem post untuk PT AC MILAN",
+        tantangan: "ajfkajfgn",
+        pelajaran: "afaggggzhasdf",
+        link: ""
+    },
+};
